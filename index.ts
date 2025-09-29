adcdc55672b070dcdb9e468373a6b3aee7ec161f
Aran Mason <contact@aran.nz>
2025-09-29 21:15:30 +0100

    Adding some production protection

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import morgan from 'morgan'
 import router from './routes/index'
 import session from 'express-session'
 import path from 'path'
+import compression from 'compression'
+import helmet from 'helmet'
+import RateLimit from 'express-rate-limit'
 
 const app = express()
 
@@ -11,7 +14,24 @@ const port: string = process.env.PORT || '8000'
 
 app.set('trust proxy', 1); // For deployment behind a proxy, e.g., Heroku
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(compression());
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and jQuery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+// Set up rate limiter: maximum of twenty requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.use(
   session({
