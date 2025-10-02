ef06946138acae5007223ae2aca0161f3d553f27
Aran Mason <contact@aran.nz>
2025-10-02 18:17:56 +0100

    URL Construction

--- routes/middleware/auth0.helper.ts
+++ routes/middleware/auth0.helper.ts
@@ -2,5 +2,8 @@ import { Request } from 'express';
 
 export function getDbUserId(req: Request): number | undefined {
     console.log('Request user:', req.oidc.user);
-    return req.oidc.user?.[`${process.env.BASE_URL}dbUserId`];
+    // Construct custom claim URL using URL constructor
+    const baseUrl = process.env.BASE_URL || 'http://localhost:8000/';
+    const claimUrl = new URL('dbUserId', baseUrl).toString();
+    return req.oidc.user?.[claimUrl];
 }
