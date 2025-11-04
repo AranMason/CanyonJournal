08b5524210a69c9570522465ccdf66316bd990ae
Aran Mason <contact@aran.nz>
2025-11-04 18:39:03 +0000

    Automating the redirect on the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { Box, Card, CardContent, Typography, Divider, Button, Alert } from '@mui/material';
 import { useUser } from '../App';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../components/Loader';
@@ -11,37 +10,15 @@ const HomePage: React.FC = () => {
   useEffect(() => {
     if (!loading && user) {
       navigate('/dashboard'); // Redirect to Dashboard (home page)
+    } else if(!loading && !user) {
+      // If not logged in, redirect to login with Auth0
+      window.location.href = '/api/login'
     }
   }, [user, loading, navigate]);
 
   return (
     <Loader isLoading={loading}>
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" bgcolor="#dadadaff" flex={1}>
-        <Card sx={{ minWidth: 340, maxWidth: 400, p: 2, boxShadow: 3 }}>
-          <CardContent>
-            <Typography variant="h5" align="center" gutterBottom>
-              Welcome to Canyon Journal
-            </Typography>
-            <Typography variant="body1" align="center" gutterBottom>
-              Your home for documenting all your canyoning adventures across the UK.
-            </Typography>
-            <Alert severity="warning" sx={{ mb: 2 }}>
-              This is currently a work in progress. Please keep that in mind while using the app.
-            </Alert>
-            <Divider sx={{ my: 2 }} />
-            <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              onClick={() => {
-                window.location.href = '/api/login';
-              }}
-            >
-              Get Started
-            </Button>
-          </CardContent>
-        </Card>
-      </Box>
+      <div>You will be redirected soon</div>
     </Loader>
   );
 };
