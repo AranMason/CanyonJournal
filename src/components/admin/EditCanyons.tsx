25877f44b4db010a2f6fa3fd4cfc30c31baf7634
Aran Mason <jobs@aran.nz>
2026-08-02 16:06:33 +0100

    Fixing admin panel

--- src/components/admin/EditCanyons.tsx
+++ src/components/admin/EditCanyons.tsx
@@ -12,7 +12,7 @@ import CanyonTypeTableCell from '../table/CanyonTypeCell';
 import CanyonNameTableCell from '../table/CanyonNameCell';
 import FilterPanel, { FilterValues } from '../FilterPanel';
 import {
-  getCanyonNameFilterConfig, getDataSourceConfig, getRegionFilterConfig, getVerifiedFilterConfig,
+    getCanyonNameFilterConfig, getDataSourceConfig, getRegionFilterConfig, getVerifiedFilterConfig,
 } from '../../helpers/filterConfigs';
 import { useTranslation } from 'react-i18next';
 import { GetRegionDisplayName } from '../../helpers/RegionHelper';
@@ -51,59 +51,53 @@ const EditCanyons: React.FC = () => {
     }, []);
 
     return <>
-    <AddCanyonModal canyon={null} open={addOpen} onClose={() => setAddOpen(false)} onSuccess={refresh} title={t('admin.addCanyon')} showSource />
-    <AddCanyonModal canyon={editCanyon} open={editCanyon != null} onClose={() => {setEditCanyon(null)}} onSuccess={refresh} title={t('settings.editCanyon')} showSource />
-    <Box display="flex" justifyContent="flex-end" sx={{ mb: 2 }}>
-        <Button variant="contained" startIcon={<AddIcon />} onClick={() => setAddOpen(true)}>
-            {t('admin.addCanyon')}
-        </Button>
-    </Box>
-    <FilterPanel<Canyon>
-        items={canyons}
-        config={[getCanyonNameFilterConfig(), getRegionFilterConfig(),  getDataSourceConfig(), getVerifiedFilterConfig(),]}
-        filterFn={filterFn}
-    >
-        {(filteredCanyons) => <TableContainer component={Paper}>
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>{t('common:fields.name')}</TableCell>
-                    <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>{t('common:fields.grade')}</TableCell>
-                    <TableCell sx={{ display: { xs: 'none', sm: 'table-cell' } }}>{t('common:fields.region')}</TableCell>
-                    <TableCell sx={{ display: { xs: 'none', sm: 'table-cell' } }}>{t('common:canyon.canyonType')}</TableCell>
-                    <TableCell>{t('common:actions.edit')}</TableCell>
-                    <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>{t('common:fields.verified')}</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {filteredCanyons.map(canyon => (
-                    <TableRow key={canyon.Id}>
-                        <CanyonNameTableCell name={canyon.Name} detailUrl={canyon.Id ? `/canyons/${canyon.Id}` : null} />
-                        <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>
-                            <CanyonRating
-                                aquaticRating={canyon.AquaticRating}
-                                verticalRating={canyon.VerticalRating}
-                                commitmentRating={canyon.CommitmentRating}
-                                starRating={canyon.StarRating}
-                                isUnrated={canyon.IsUnrated}
-                            />
-                        </TableCell>
-                        <TableCell sx={{ display: { xs: 'none', sm: 'table-cell' } }}>
-                            <RegionIcon regionSlug={canyon.RegionSlug ?? ''} regionSymbol={canyon.RegionSymbol} size={16} />&nbsp;{GetRegionDisplayName(canyon.RegionSlug)}
-                        </TableCell>
-                        <CanyonTypeTableCell type={canyon.CanyonType} sx={{ display: { xs: 'none', sm: 'table-cell' } }} />
-                        <TableCell>
-                            <RowActions onEdit={() => setEditCanyon(canyon)} /> 
-                        </TableCell>
-                        <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>
-                            {canyon.IsVerified ? <CheckIcon color='success'/> : <CloseIcon color='error'/>}
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </TableContainer>}
-    </FilterPanel></>;
+        <AddCanyonModal canyon={null} open={addOpen} onClose={() => setAddOpen(false)} onSuccess={refresh} title={t('admin.addCanyon')} showSource />
+        <AddCanyonModal canyon={editCanyon} open={editCanyon != null} onClose={() => { setEditCanyon(null) }} onSuccess={refresh} title={t('settings.editCanyon')} showSource />
+        <Box display="flex" justifyContent="flex-end" sx={{ mb: 2 }}>
+            <Button variant="contained" startIcon={<AddIcon />} onClick={() => setAddOpen(true)}>
+                {t('admin.addCanyon')}
+            </Button>
+        </Box>
+        <FilterPanel<Canyon>
+            items={canyons}
+            config={[getCanyonNameFilterConfig(), getRegionFilterConfig(), getDataSourceConfig(), getVerifiedFilterConfig(),]}
+            filterFn={filterFn}
+        >
+            {(filteredCanyons) => <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>{t('common:fields.name')}</TableCell>
+                            <TableCell sx={{ display: { xs: 'none', sm: 'table-cell' } }}>{t('common:canyon.canyonType')}</TableCell>
+                            <TableCell>{t('common:actions.edit')}</TableCell>
+                            <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>{t('common:fields.verified')}</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {filteredCanyons.map(canyon => (
+                            <TableRow key={canyon.Id}>
+                                <TableCell>
+                                    <CanyonNameTableCell canyon={canyon} detailUrl={canyon.Id ? `/canyons/${canyon.Id}` : null} subtitle={<CanyonRating
+                                        aquaticRating={canyon.AquaticRating}
+                                        verticalRating={canyon.VerticalRating}
+                                        commitmentRating={canyon.CommitmentRating}
+                                        starRating={canyon.StarRating}
+                                        isUnrated={canyon.IsUnrated}
+                                    />} />
+                                </TableCell>
+                                <CanyonTypeTableCell type={canyon.CanyonType} sx={{ display: { xs: 'none', sm: 'table-cell' } }} />
+                                <TableCell>
+                                    <RowActions onEdit={() => setEditCanyon(canyon)} />
+                                </TableCell>
+                                <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>
+                                    {canyon.IsVerified ? <CheckIcon color='success' /> : <CloseIcon color='error' />}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>}
+        </FilterPanel></>;
 }
 
 export default EditCanyons;
