aaf867d729b772d0babc172adb3dbb60aaef3b21
Aran Mason <jobs@aran.nz>
2026-09-05 19:23:47 +0100

    Adding font weight in mobile

--- src/components/MobileAppBar.tsx
+++ src/components/MobileAppBar.tsx
@@ -22,11 +22,11 @@ const MobileAppBar: React.FC<MobileAppBarProps> = ({ onMenuClick }) => {
 
         <Link to="/dashboard" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
           <img src="/favicon.svg" alt={t('common:app.name')} style={{ width: 28, height: 28, marginRight: 8 }} />
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant="h6" noWrap component="div" fontWeight={700}>
             {t('common:app.name')}
           </Typography>
         </Link>
-        
+
       </Toolbar>
     </AppBar>
   );
